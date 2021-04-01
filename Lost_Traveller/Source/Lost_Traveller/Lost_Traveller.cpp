b551b0ab12d4ccbe00687a92bb7defa1aa70c11c
Alissa0101 <30415619+itzbradmc@users.noreply.github.com>
2021-04-02 00:57:10 +0100

    First attempt
    Not idea what's even going on
    Why do we have to use blueprints they make it so much harder

---
+++ Lost_Traveller/Source/Lost_Traveller/Lost_Traveller.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lost_Traveller.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lost_Traveller, "Lost_Traveller" );
